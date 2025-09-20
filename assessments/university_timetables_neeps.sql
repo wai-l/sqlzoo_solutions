b12eece4aa0c6ae64937e99afd5448bc042a57a4
to-waiting-room <to.waiting.room@gmail.com>
2025-09-20 09:08:59 +0100

    Changes to be committed: 	modified:   assessments/university_timetables_neeps.sql

--- assessments/university_timetables_neeps.sql
+++ assessments/university_timetables_neeps.sql
@@ -84,3 +84,102 @@ FROM event AS e
 INNER JOIN event_co72010 AS c
 ON e.dow = c.dow AND e.tod = c.tod
 WHERE e.modle <> 'co72010';
+
+-- 10. How many members of staff have contact time which is greater than the average?
+-- this will include all staff including those with no event
+-- average contact time is defined as the number of hour the staffs teaches an event
+-- staff with no associted events will have a contact time of 0 and included in the average
+-- there are event associated with more than 1 staff; in those cases all staffs assocaited are getting full credit for the contact hours
+-- e.g. if there is an event that last for 1 hour and associated with 3 staffs, each staff will have a contact hour of 1 from that event
+
+-- solution 1
+-- result: 31
+WITH 
+total_contact_by_staff AS (
+    SELECT 
+        staff.id AS staff_id, 
+        staff.name AS staff_name, 
+        COALESCE(SUM(event.duration), 0) AS total_contact_hours
+    FROM staff 
+    LEFT JOIN teaches ON staff.id = teaches.staff
+    LEFT JOIN event ON teaches.event = event.id
+    GROUP BY staff.id, staff.name
+), 
+avg_contact_by_staff AS (
+    SELECT AVG(total_contact_hours) AS avg_contact_hours
+    FROM total_contact_by_staff
+)
+SELECT COUNT(DISTINCT staff_id)
+FROM total_contact_by_staff
+WHERE total_contact_hours > (SELECT avg_contact_hours FROM avg_contact_by_staff); 
+
+-- solution 2: remove the 2nd CTE
+-- result: 31
+WITH 
+total_contact_by_staff AS (
+    SELECT 
+        staff.id AS staff_id, 
+        staff.name AS staff_name, 
+        COALESCE(SUM(event.duration), 0) AS total_contact_hours
+    FROM staff 
+    LEFT JOIN teaches ON staff.id = teaches.staff
+    LEFT JOIN event ON teaches.event = event.id
+    GROUP BY staff.id, staff.name
+)
+SELECT COUNT(DISTINCT staff_id)
+FROM total_contact_by_staff
+WHERE total_contact_hours > (SELECT AVG(total_contact_hours) FROM total_contact_by_staff); 
+
+-- solution 3: this only count staff that have events associated with them
+-- result: 17
+WITH 
+total_contact_by_staff AS (
+    SELECT 
+        teaches.staff AS staff_id, 
+        -- staff.name AS staff_name, 
+        COALESCE(SUM(event.duration), 0) AS total_contact_hours
+    FROM teaches
+    -- LEFT JOIN teaches ON staff.id = teaches.staff
+    LEFT JOIN event ON teaches.event = event.id
+    GROUP BY teaches.staff
+)
+SELECT COUNT(DISTINCT staff_id)
+FROM total_contact_by_staff
+WHERE total_contact_hours > (SELECT AVG(total_contact_hours) FROM total_contact_by_staff); 
+
+-- 11. co.CHt is to be given all the teaching that co.ACg currently does. Identify those events which will clash.
+WITH 
+schedule AS (
+    SELECT 
+        teaches.staff, 
+        event.id, 
+        event.dow, 
+        event.tod, 
+        CAST(SUBSTR(event.tod,1,2) AS INT) AS start_hour, -- all events start on the hour; this is an alternative as the time couldn't be parsed as time or datetime in sqlzoo
+        CAST(SUBSTR(event.tod,1,2) AS INT) + event.duration AS end_hour, 
+        occurs.week
+        FROM event 
+    LEFT JOIN teaches ON event.id = teaches.event
+    LEFT JOIN occurs ON event.id = occurs.event
+    WHERE teaches.staff IN ('co.CHT', 'co.ACg')
+    ), 
+cht AS (
+    SELECT * 
+    FROM schedule
+    WHERE staff = 'co.CHT'
+    ),
+acg AS (  
+    SELECT * 
+    FROM schedule
+    WHERE staff = 'co.ACg'
+    )
+SELECT DISTINCT
+    cht.id AS cht_id, 
+    acg.id AS acg_id
+FROM cht
+INNER JOIN acg
+ON 
+    cht.dow = acg.dow
+    AND cht.week = acg.week
+    AND cht.start_hour < acg.end_hour
+    AND acg.start_hour < cht.end_hour;
